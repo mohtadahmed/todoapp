16ae5781a9cc8ca9870a4f4805dfc13d17c3d738
Mohtad Ahmed Saeem <mohtadsaeem13@gmail.com>
2022-01-11 00:23:44 +0600

    DataBase created

--- app.py
+++ app.py
@@ -1,8 +1,22 @@
-from flask import Flask
+from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
 
 
 @app.route('/')
 def index():
-    return "Hello World"
+    return render_template('base.html')
+
+
+@app.route("/about")
+def about():
+    return "About Page"
+
+
+if __name__ == "__main__":
+    db.create_all()
+    app.run(debug=True)
